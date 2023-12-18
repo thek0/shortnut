d5d01fe6578358b3c4947fb059d1e67bb464c7e6
yish0 <ca01952@cartahd.com>
2023-12-18 23:55:43 +0900

    serve sveltedev

--- electron/index.ts
+++ electron/index.ts
@@ -13,8 +13,8 @@ import path from "path";
 import { pathToFileURL } from "node:url";
 
 const developerOptions = {
-  isInProduction: true, // true if is in production
-  serveSvelteDev: false, // true when you want to watch svelte
+  isInProduction: false, // true if is in production
+  serveSvelteDev: true, // true when you want to watch svelte
   buildSvelteDev: false, // true when you want to build svelte
   watchSvelteBuild: false, // true when you want to watch build svelte
 };
